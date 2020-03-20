a317251d42aa78531dc86c3a3a369014ed08054f
trano <trano@DESKTOP-CUTEDA4>
2020-03-20 18:24:53 +0900

    Create a Menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("| Music PlayList |\n");
+		System.out.println("** Select a menu **");
+		System.out.println("  1. Add Music");
+		System.out.println("  2. Delete Music");
+		System.out.println("  3. Edit Music");
+		System.out.println("  4. View Music List");
+		System.out.println("  5. Exit");
+	}
+}
